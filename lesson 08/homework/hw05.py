6b3e81ef90d314ba4312ce54600718678bb4b94b
achi Xozrevanidze <dachixozrevaniidze@gmail.com>
2025-10-25 20:15:37 +0400

    second commit

---
+++ lesson 08/homework/hw05.py
@@ -0,0 +1,17 @@
+# 5) მომხმარებელს შემოატანინე:
+# --> მომხმარებლის სახელი (username)
+# --> პაროლი (password)
+# შეამოწმე:
+# თუ მომხმარებელი არის "admin" და პაროლი არის 'superSecretPassword' → "მოგესალმები, ადმინ!"
+# თუ მომხმარებელი "guest" და პაროლი არის "1234" → "მოგესალმები, სტუმარო!"
+# სხვა შემთხვევაში → "მომხმარებელი არ მოიძებნა!"
+
+username =input ("enter your username")
+password =input ("enter your password")
+
+if username == "admin" and password == "superSecretPassword":
+    print("მოგესალმები, ადმინ")
+elif username == "guest" and password == "1234":
+    print("მოგესალმები, სტუმარო")
+else:
+    print ("მომხმარებელი არ მოიძებნა")
